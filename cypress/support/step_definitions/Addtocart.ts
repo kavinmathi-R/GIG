a9735e893936e0c2d6096ce214b924d85e363542
kavinmathi-R <61658940+kavinmathi-R@users.noreply.github.com>
2020-03-04 14:13:12 +0100

    Update Addtocart.ts
    Commit

--- cypress/support/step_definitions/Addtocart.ts
+++ cypress/support/step_definitions/Addtocart.ts
@@ -10,7 +10,7 @@ const {
 Given('I am in the automation practice home page and i serach for black dresses',()=>{
 
     cy.visit("www.automationpractice.com");
-    cy.get('#search_query_top').type('Black dress');
+    cy.get('#search_query_top').type('Black dress');    //Searching for Black dress
     cy.screenshot();
     cy.get('#searchbox > .btn').click();
   
@@ -18,13 +18,13 @@ Given('I am in the automation practice home page and i serach for black dresses'
   
   when('I select the first two black dresses and increase the quantity of second dress and I remove the first dress form the cart',()=>{
   
-   cy.get('.ajax_add_to_cart_button[data-id-product="5"]').click();
-    cy.get('.continue > span').click();
-   cy.get('.ajax_add_to_cart_button[data-id-product="4"]').click();
-   cy.get('.btn-default[title = "Proceed to checkout" ]').click();
-    cy.get('[name="quantity_4_16_0_0"],[value="3"]').clear().type('2');
-    cy.get('#cart_quantity_down_5_19_0_0 ').click();
-    cy.screenshot();
+   cy.get('.ajax_add_to_cart_button[data-id-product="5"]').click();     //Adding first black dress in the cart
+   cy.get('.continue > span').click();
+   cy.get('.ajax_add_to_cart_button[data-id-product="4"]').click();     //Adding second black dress in the cart
+   cy.get('.btn-default[title = "Proceed to checkout" ]').click();      
+   cy.get('[name="quantity_4_16_0_0"],[value="3"]').clear().type('2');    //Increasing the quantity of First balck dress
+   cy.get('#cart_quantity_down_5_19_0_0 ').click();                       //Reducing the qantity of first dress
+   cy.screenshot();
   })
   
   
