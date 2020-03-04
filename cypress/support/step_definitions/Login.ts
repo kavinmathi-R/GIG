84c9813dbac7ecf981279083b19f01d564936158
kavinmathi-R <61658940+kavinmathi-R@users.noreply.github.com>
2020-03-04 14:15:25 +0100

    commit

--- cypress/support/step_definitions/Login.ts
+++ cypress/support/step_definitions/Login.ts
@@ -9,21 +9,21 @@ const {
 } = require("cypress-cucumber-preprocessor/steps");
 
 Given(`I am in the automation practice home page and i click sign in`, () => {
-  cy.visit("www.automationpractice.com");
-  cy.get(".login[title='Log in to your customer account']").click();
+  cy.visit("www.automationpractice.com");                                   //Navigate to the URL
+  cy.get(".login[title='Log in to your customer account']").click();        //Click on Signin
   cy.screenshot();
 });
 
 when('I enter valid cemail address and password',()=> {
-  cy.get("#email").type("AutomationDemo@gmail.com");
-    cy.get("#passwd").type("Demo2103");
+  cy.get("#email").type("AutomationDemo@gmail.com");                      //Enter the appropriate email address
+    cy.get("#passwd").type("Demo2103");                                   //Enter the valid pwassword
     cy.get('#SubmitLogin > span').click();
     cy.screenshot();
 });
 
 Then(`I should be able to login successfully`, () => {
   myAssertion();
-  cy.get('.page-heading').should('contain','My account');
+  cy.get('.page-heading').should('contain','My account');                 //Validating whether the customer is logged in successfully 
   cy.screenshot();
 });
 
